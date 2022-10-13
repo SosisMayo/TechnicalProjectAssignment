b78d9f3f054d23c945fa0fc055b69915afd89efc
sosismayo <ramadani.dwicahya@gmail.com>
2022-10-13 16:41:14 +0700

    TMDB add page control

--- TMDB/home/index.js
+++ TMDB/home/index.js
@@ -1,17 +1,16 @@
 const containerFilm = document.querySelector('.container-film');
-const page = 1;
+const nextPage = document.querySelector('.next-page');
+const previousPage = document.querySelector('.previous-page');
+const angkaPertama = document.querySelector('.angka-pertama');
+const angkaKedua = document.querySelector('.angka-kedua');
+const angkaKetiga = document.querySelector('.angka-ketiga');
+const angkaKeempat = document.querySelector('.angka-keempat');
+const angka = document.querySelector('.angka');
+let page = 1;
 let data;
 let dataSearch;
 let query;
 let search = document.querySelector('#search');
-
-search.addEventListener('keypress',async (e)=>{
-    if(e.key === 'Enter'){
-        query = search.value;
-        e.preventDefault();
-        await searchFilm();
-    }
-})
 
 const discoverFilm = async () => {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=2d9decd98bb8289f2a7d3c030315fcb1&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page} &with_watch_monetization_types=flatrate`);
@@ -27,11 +26,17 @@ const searchFilm = async () => {
     while(containerFilm.firstChild){
         containerFilm.removeChild(containerFilm.firstChild);
     }
-    makeCardSearch();
+    if(dataSearch.length === 0){
+        const noResult = document.createElement('h1');
+        document.querySelector(".page-control").style.display = "none";
+        noResult.textContent = 'No Result';
+        containerFilm.appendChild(noResult);
+    }else{
+        makeCardSearch();
+    }
 }
 
 const makeCardSearch = async () => {
-    await discoverFilm();
     containerFilm.innerHTML = dataSearch.map(film => {
         return `
         <div class="film-item">
@@ -63,5 +68,77 @@ const makeCard = async () => {
         </div>`
     }).join('');
 }
+
+const nextPageHandler = async () => {
+    if (page < 99){
+        page++;
+        await makeCard();
+        updateHalaman();
+
+    }
+    else{
+        page = 1;
+        await makeCard();
+        updateHalaman();
+    }
+}
+
+const previousPageHandler = async () => {
+    if (page === 1){
+        page = 99;
+        console.log(page);
+        await makeCard();
+        updateHalaman();
+    }
+    else{
+        page--;
+        await makeCard();
+        updateHalaman();
+    }
+}
+
+const updateHalaman = () => {
+    const titik = document.querySelector('.titik');
+    const titik2 = document.querySelector('.titik2');
+    
+    if(page < 4){
+        titik.style.display = "none";
+        titik2.style.display = "block";
+        angkaPertama.textContent = 1;
+        angkaKedua.textContent = 2;
+        angkaKetiga.textContent = 3;
+        angkaKeempat.textContent = 4;
+    }
+
+    else if(page > 95){
+        console.log(page);
+        titik.style.display = "block";
+        titik2.style.display = "none";
+        angkaPertama.textContent = 1;
+        angkaKedua.textContent = 96;
+        angkaKetiga.textContent = 97;
+        angkaKeempat.textContent = 98;
+    }
+    else{
+        titik.style.display = "block";
+        titik2.style.display = "block";
+        angkaPertama.textContent = 1;
+        angkaKedua.textContent = page - 1;
+        angkaKetiga.textContent = page;
+        angkaKeempat.textContent = page + 1;
+    }
+    
+}
+
+search.addEventListener('keypress',async (e)=>{
+    if(e.key === 'Enter'){
+        query = search.value;
+        e.preventDefault();
+        await searchFilm();
+    }
+})
+
+nextPage.addEventListener('click', nextPageHandler);
+previousPage.addEventListener('click', previousPageHandler);
 
 makeCard();
